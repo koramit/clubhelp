2959876569a0a2eef596aa2c9ee1a2dd67baa15f
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-14 10:25:19 +0700

    design logo

---
+++ resources/views/logo.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Logo</title>
+    <link href="https://fonts.googleapis.com/css2?family=Fascinate+Inline&family=Mali:wght@300&display=swap"
+        rel="stylesheet">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+</head>
+
+<body class="m-0 bg-soft-theme-light font-fascinate-inline">
+    <div class="flex justify-center items-center w-full min-h-screen">
+        <div
+            class="flex flex-col justify-center items-center w-80 h-80 rounded-full bg-bitter-theme-light text-dark-theme-light text-8xl">
+            <div class=" text-thick-theme-light mb-2">Club</div>
+            <div class=" text-soft-theme-light mt-2">HELP</div>
+        </div>
+    </div>
+</body>
+
+</html>
