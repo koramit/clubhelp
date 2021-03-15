193904a5af202da92306929d8139fbb9368900ef
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-15 11:44:10 +0700

    implement add bot

--- app/Http/Controllers/Services/TelegramWebhooksController.php
+++ app/Http/Controllers/Services/TelegramWebhooksController.php
@@ -3,16 +3,65 @@
 namespace App\Http\Controllers\Services;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 
 class TelegramWebhooksController extends Controller
 {
+    protected $update;
+    protected $baseEndpoint;
+
     public function __invoke($token)
     {
         if ($token !== config('services.telegram.bot_token')) {
             abort(404);
         }
-        Log::info(['app' => 'Telegram', 'payload' => Request::all()]);
+
+        $this->baseEndpoint = 'https://api.telegram.org/bot'.config('services.telegram.bot_token').'/';
+
+        $this->update = Request::all();
+        Log::info($this->update);
+
+        if (isset($this->update['message'])) { // message and add bot
+            if (isset($this->update['text'])) { // text message
+                if ($this->update['text'] === '/start' ||
+                    $this->update['text'] === '/restart'
+                ) {
+                    $this->handleSubscript();
+                }
+                $this->handleTextMessage();
+            }
+        } elseif (isset($this->update['my_chat_member'])) { // blocked/unblock
+        }
+    }
+
+    protected function handleSubscript()
+    {
+        $user = User::where('profile->social->id', $this->update['chat']['id'])->first();
+
+        if (! $user) {
+            $response = Http::post("{$this->baseEndpoint}sendMessage", [
+                'chat_id' => $this->update['chat']['id'],
+                'text' => str_replace('PLACEHOLDER', $this->update['chat']['username'], config('messages.bot_user_not_registred')),
+            ]);
+        }
+
+        if ($user->getNotificationChannel() === null) {
+            $user->setNotificationChannel('telegram', $this->update['chat']['id']);
+            $response = Http::post("{$this->baseEndpoint}sendMessage", [
+                'chat_id' => $this->update['chat']['id'],
+                'text' => str_replace('PLACEHOLDER', $user->profile['full_name'], config('messages.bot_greeting')),
+            ]);
+        }
+    }
+
+    protected function handleTextMessage()
+    {
+        $response = Http::post("{$this->baseEndpoint}sendMessage", [
+            'chad_id' => $this->update['chat']['id'],
+            'text' => strrev($this->update['text']),
+        ]);
     }
 }
